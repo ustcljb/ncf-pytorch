4860fd41c4dad1f2d2a64339b7bc2f75f3cd1cd3
ustcljb <ustcljb@gmail.com>
2020-05-18 22:05:01 -0400

    Add NeuMF model

---
+++ NeuMF.py
@@ -0,0 +1,200 @@
+import os
+import time
+import argparse
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import torch.backends.cudnn as cudnn
+from tensorboardX import SummaryWriter
+
+import data_util
+import evaluate
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--lr", type=float, default=0.001, help="learning rate")
+parser.add_argument("--dropout", type=float, default=0.0, help="dropout rate")
+parser.add_argument("--batch_size", type=int, default=256, help="batch size for training")
+parser.add_argument("--epochs", type=int, default=20, help="training epoches")
+parser.add_argument("--top_k", type=int, default=10, help="compute metrics@top_k")
+parser.add_argument("--embedding_dim_GMF", type=int, default=32, help="dimension of embedding in GMF submodel")
+parser.add_argument("--embedding_dim_MLP", type=int, default=128, help="dimension of embedding in MLP submodel")
+parser.add_argument("--hidden_layer", type=list, default=[128, 64, 32], help="dimension of each hidden layer in MLP")
+parser.add_argument("--use_pretrained", action="store_true", help="use pretrained model to initialize weights")
+parser.add_argument("--num_ng", type=int, default=4, help="sample negative items for training")
+parser.add_argument("--test_num_ng", type=int, default=99, help="sample part of negative items for testing")
+parser.add_argument("--data_set", type=str, default="ml-1m", help="data set. 'ml-1m' or 'pinterest-20'")
+parser.add_argument("--data_path", type=str, default="/Users/JingboLiu/Desktop/ncf_pytorch/data")
+parser.add_argument("--model_path", type=str, default="/Users/JingboLiu/Desktop/ncf_pytorch/model")
+parser.add_argument("--out", default=True, help="save model or not")
+parser.add_argument("--disable-cuda", action="store_true", help="Disable CUDA")
+args = parser.parse_args()
+
+args.device = None
+if not args.disable_cuda and torch.cuda.is_available():
+    args.device = torch.device('cuda')
+else:
+    args.device = torch.device('cpu')
+
+cudnn.benchmark = True
+
+
+class NeuMF(nn.Module):
+    def __init__(self, user_num, item_num, embedding_dim_GMF, embedding_dim_MLP, hidden_layer_MLP,
+                 dropout, GMF_model=None, MLP_model=None):
+        super(NeuMF, self).__init__()
+        """
+        user_num: number of users;
+        item_num: number of items;
+        embedding_dim_GMF: number of embedding dimensions in GMF submodel;
+        embedding_dim_MLP: number of embedding dimensions in MLP submodel;
+        hidden_layer_MLP: dimension of each hidden layer (list type) in MLP submodel;
+        dropout: dropout rate between fully connected layers;
+        GMF_model: pre-trained GMF weights;
+		MLP_model: pre-trained MLP weights.
+        """
+        self.dropout = dropout
+        self.GMF_model = GMF_model
+        self.MLP_model = MLP_model
+
+        self.embed_user_GMF = nn.Embedding(user_num, embedding_dim_GMF)
+        self.embed_item_GMF = nn.Embedding(item_num, embedding_dim_GMF)
+        self.embed_user_MLP = nn.Embedding(user_num, embedding_dim_MLP)
+        self.embed_item_MLP = nn.Embedding(item_num, embedding_dim_MLP)
+
+
+        MLP_modules = []
+        self.num_layers = len(hidden_layer_MLP)
+        for i in range(self.num_layers):
+            MLP_modules.append(nn.Dropout(p=self.dropout))
+            if i == 0:
+                MLP_modules.append(nn.Linear(embedding_dim_MLP*2, hidden_layer_MLP[0]))
+            else:
+                MLP_modules.append(nn.Linear(hidden_layer_MLP[i-1], hidden_layer_MLP[i]))
+            MLP_modules.append(nn.ReLU())
+        self.MLP_layers = nn.Sequential(*MLP_modules)
+
+        self.predict_layer = nn.Linear(hidden_layer_MLP[-1] + embedding_dim_GMF, 1)
+
+        if not args.use_pretrained:
+            nn.init.normal_(self.embed_user_GMF.weight, std=0.01)
+            nn.init.normal_(self.embed_item_GMF.weight, std=0.01)
+            nn.init.normal_(self.embed_user_MLP.weight, std=0.01)
+            nn.init.normal_(self.embed_item_MLP.weight, std=0.01)
+
+            for m in self.MLP_layers:
+                if isinstance(m, nn.Linear):
+                    nn.init.xavier_uniform_(m.weight)
+            nn.init.kaiming_uniform_(self.predict_layer.weight, a=1, nonlinearity='sigmoid')
+
+
+            # Kaiming/Xavier initialization can not deal with non-zero bias terms
+            for m in self.modules():
+                if isinstance(m, nn.Linear) and m.bias is not None:
+                    m.bias.data.zero_()
+        else:
+            # embedding layers
+            self.embed_user_GMF.weight.data.copy_(self.GMF_model.embed_user.weight)
+            self.embed_item_GMF.weight.data.copy_(self.GMF_model.embed_item.weight)
+            self.embed_user_MLP.weight.data.copy_(self.MLP_model.embed_user.weight)
+            self.embed_item_MLP.weight.data.copy_(self.MLP_model.embed_item.weight)
+
+            # mlp layers
+            for (m1, m2) in zip(self.MLP_layers, self.MLP_model.MLP_layers):
+                if isinstance(m1, nn.Linear) and isinstance(m2, nn.Linear):
+                    m1.weight.data.copy_(m2.weight)
+                    m1.bias.data.copy_(m2.bias)
+
+            # predict layers
+            predict_weight = torch.cat([
+                self.GMF_model.predict_layer.weight,
+                self.MLP_model.predict_layer.weight], dim=1)
+            precit_bias = self.GMF_model.predict_layer.bias + self.MLP_model.predict_layer.bias
+
+            self.predict_layer.weight.data.copy_(0.5 * predict_weight)
+            self.predict_layer.bias.data.copy_(0.5 * precit_bias)
+
+    def forward(self, user, item):
+        embed_user_GMF = self.embed_user_GMF(user)
+        embed_item_GMF = self.embed_item_GMF(item)
+        output_GMF = embed_user_GMF * embed_item_GMF
+
+        embed_user_MLP = self.embed_user_MLP(user)
+        embed_item_MLP = self.embed_item_MLP(item)
+        interaction = torch.cat((embed_user_MLP, embed_item_MLP), -1)
+        output_MLP = self.MLP_layers(interaction)
+
+        concat = torch.cat((output_GMF, output_MLP), -1)
+
+        prediction = self.predict_layer(concat)
+        return prediction.view(-1)
+
+if __name__=="__main__":
+    data_file = os.path.join(args.data_path, args.data_set)
+    train_data, test_data, user_num, item_num, train_mat = data_util.load_all(data_file)
+
+    train_dataset = data_util.NCFData(train_data, item_num, train_mat, args.num_ng, True)
+    test_dataset = data_util.NCFData(test_data, item_num, train_mat, 0, False)
+    train_loader = data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
+    test_loader = data.DataLoader(test_dataset, batch_size=args.test_num_ng + 1, shuffle=False, num_workers=0)
+
+    GMF_model_path = os.path.join(args.model_path, 'GMF.pth')
+    MLP_model_path = os.path.join(args.model_path, 'MLP.pth')
+    if args.use_pretrained:
+        assert os.path.exists(GMF_model_path), 'lack of GMF model'
+        assert os.path.exists(MLP_model_path), 'lack of MLP model'
+        GMF_model = torch.load(GMF_model_path)
+        MLP_model = torch.load(MLP_model_path)
+    else:
+        GMF_model = None
+        MLP_model = None
+    model = NeuMF(user_num, item_num, args.embedding_dim_GMF, args.embedding_dim_MLP,
+                  args.hidden_layer_MLP, args.dropout, GMF_model, MLP_model)
+    model.to(device=args.device)
+    loss_function = nn.BCEWithLogitsLoss()
+
+    if args.use_pretrained:
+        optimizer = optim.SGD(model.parameters(), lr=args.lr)
+    else:
+        optimizer = optim.Adam(model.parameters(), lr=args.lr)
+
+    # writer = SummaryWriter() # for visualization
+
+    ########################### TRAINING #####################################
+    count, best_hr = 0, 0
+    for epoch in range(args.epochs):
+        model.train()  # Enable dropout (if have).
+        start_time = time.time()
+        train_loader.dataset.ng_sample()
+
+        for user, item, label in train_loader:
+            user = user.to(device=args.device)
+            item = item.to(device=args.device)
+            label = label.float().to(device=args.device)
+
+            model.zero_grad()
+            prediction = model(user, item)
+            loss = loss_function(prediction, label)
+            loss.backward()
+            optimizer.step()
+            # writer.add_scalar('data/loss', loss.item(), count)
+            count += 1
+
+        model.eval()
+        HR, NDCG = evaluate.metrics(model, test_loader, args.top_k, args.device)
+
+        elapsed_time = time.time() - start_time
+        print("The time elapse of epoch {:03d}".format(epoch) + " is: " +
+              time.strftime("%H: %M: %S", time.gmtime(elapsed_time)))
+        print("HR: {:.3f}\tNDCG: {:.3f}".format(np.mean(HR), np.mean(NDCG)))
+
+        if HR > best_hr:
+            best_hr, best_ndcg, best_epoch = HR, NDCG, epoch
+            if args.out:
+                if not os.path.exists(args.model_path):
+                    os.mkdir(args.model_path)
+                torch.save(model, os.path.join(args.model_path, 'MLP.pth'))
+
+    print("End. Best epoch {:03d}: HR = {:.3f}, NDCG = {:.3f}".format(best_epoch, best_hr, best_ndcg))
